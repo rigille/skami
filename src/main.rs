f3637dab3c9492cfc2fc89f5e95539f7c34e57fe
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2022-03-28 09:59:32 -0300

    include tui-rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    Ok(())
 }
